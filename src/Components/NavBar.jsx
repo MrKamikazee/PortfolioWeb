0fa434a00ebd13c83dca8e42c0e1625792c9ab73
MrKamikazee <martinroma001@gmail.com>
2025-05-10 16:19:28 -0300

    Start Point

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+
+function NavBar() {
+    return (
+        <div className="NavBar">
+            <h1 className="Tittle">Portfolio</h1>
+        </div>
+    );
+}
+
+export default NavBar;
---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import './CSS/App.css';
+import NavBar from "./Components/NavBar";
+
+function Main() {
+  return (
+    <div className="App">
+      <header className="Header">
+        <NavBar />
+      </header>
+    </div>
+  );
+}
+
+export default Main;
